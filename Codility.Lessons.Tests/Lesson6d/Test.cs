fde31c61edb3acfc3eafc81ffab221995000b68f
shoter <lubiebardzozelki@gmail.com>
2020-05-23 21:18:40 +0200

    l6d

---
+++ Codility.Lessons.Tests/Lesson6d/Test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Lessons.Tests.Lesson6d
+{
+    public class Test
+    {
+        Codility.Lessons.Lesson6d.Solution s = new Lessons.Lesson6d.Solution();
+
+        [Theory]
+        [InlineData(new int[] { 10, 2, 5, 1, 8, 20 }, 1)]
+        [InlineData(new int[] { int.MaxValue, int.MaxValue - 1, int.MaxValue - 2}, 1)]
+        [InlineData(new int[] { 10, 50, 5, 1}, 0)]
+        [InlineData(new int[] {1}, 0)]
+        [InlineData(new int[] { 1, 2 }, 0)]
+        [InlineData(new int[] { }, 0)]
+
+
+        public void test(int[] A ,int R)
+        {
+            Assert.Equal(R, s.solution(A));
+
+        }
+
+        [Theory]
+        [InlineData(new int[] { 10, 2, 5, 1, 8, 20 }, 1)]
+        [InlineData(new int[] { 10, 50, 5, 1 }, 0)]
+
+        public void testBrute(int[] A, int R)
+        {
+            Assert.Equal(R, s.brute(A));
+        }
+
+
+        [Fact]
+        public void autoCheck()
+        {
+            for(int x = 0; x < 1000; ++ x)
+            {
+                int size = 1000;
+                Random rand = new Random();
+                int[] A = new int[size];
+                for (int i = 0; i < A.Length; ++i)
+                    A[i] = rand.Next(-100, 100);
+
+                Assert.Equal(s.brute(A), s.solution(A));
+            }
+        }
+
+
+
+    }
+}
---
+++ Codility.Lessons/Lesson6d/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.Lesson6d
+{
+    public class Solution
+    {
+       public int solution(int[] A)
+        {
+            if (A.Length < 3)
+                return 0;
+
+            List<long> sorted = A.Select(x => (long)x).ToList();
+            sorted.Sort();
+
+            for(int i = 0; i < A.Length - 2; ++i)
+            {
+                long x = sorted[i];
+                long y = sorted[i + 1];
+                long z = sorted[i + 2];
+                if (x + y > z && x + z > y && z + y > x)
+                    return 1;
+            }
+            return 0;
+
+
+        }
+
+        public int brute(int[] A)
+        {
+            for(int x=  0; x< A.Length; ++ x)
+                for(int y= 0;y < A.Length; ++ y)
+                    for(int z = 0; z < A.Length; ++ z)
+                    {
+                        if (x == y || y == z || z == x)
+                            continue;
+
+                        if (x + y > z && x + z > y && z + y > x)
+                            return 1;
+                    }
+            return 0;
+        }
+    }
+}
