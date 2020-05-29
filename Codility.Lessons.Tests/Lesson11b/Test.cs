b67a818bc059b1649419d971bc3c26fdf2b5782d
shoter <lubiebardzozelki@gmail.com>
2020-05-30 00:53:09 +0200

    l11b

---
+++ Codility.Lessons.Tests/Lesson11b/Test.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Lessons.Tests.Lesson11b
+{
+    public class Test
+    {
+        Codility.Lessons.Lesson11b.Solution s { get; } = new Lessons.Lesson11b.Solution();
+
+        [Theory]
+        [InlineData(26, new int[] { 1, 4, 16 }, new int[] { 26, 10, 20 }, new int[] { 10, 4, 0 })]
+        public void test(int N, int[] P, int[] Q,int[] R)
+        {
+            Assert.Equal(R, s.solution(N, P, Q));
+        }
+
+        [Theory]
+        [InlineData(26, new int[] { 1, 4, 16 }, new int[] { 26, 10, 20 }, new int[] { 10, 4, 0 })]
+        public void bruteTest(int N, int[] P, int[] Q, int[] R)
+        {
+            Assert.Equal(R, s.brute(N, P, Q));
+        }
+
+        [Fact]
+        public void autoCheck()
+        {
+            for(int x = 1; x <= 100; ++ x)
+            {
+                int[] P = new int[x];
+                int[] Q = new int[x];
+                Random r = new Random();
+
+                int n = r.Next(10, 1000);
+
+                for(int i = 0;i < x; ++i)
+                {
+                    P[i] = r.Next(1, n);
+                    Q[i] = r.Next(n, n + n);
+                }
+
+                Assert.Equal(s.brute(n * 2, P, Q), s.solution(n * 2, P, Q));
+            }
+
+        }
+
+        [Theory]
+        [InlineData(8, new bool[] { false, false, true, true, false, true, false, true, false})]
+        public void sieveTest(int N, bool[] R)
+        {
+            Assert.Equal(R, s.sieve(N));
+        }
+
+        [Fact]
+        public void factorTest()
+        {
+            int[] R = s.factors(100);
+            for(int i = 2; i <= 100; ++i)
+            {
+                for(int x = 2; x <= i; ++i)
+                {
+                    if(i % x == 0)
+                    {
+                        Assert.Equal(x, R[i]);
+                        break;
+                    }
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData(true, new int[] { 4, 6, 9, 10, 14, 15, 21, 22, 25, 26 })]
+        [InlineData(false, new int[] { 1,2, 5, 7, 8, 11, 12, 13, 16, 17, 18, 19, 20, 23, 24, 27})]
+        public void IsPrimeTest(bool isPrime, int[] numbers)
+        {
+            s.sieve(1000);
+            s.factors(1000);
+            foreach (var n in numbers)
+                Assert.Equal(isPrime, s.IsPrime(n));
+        }
+
+        [Fact]
+        public void big()
+        {
+            int size = 30_000;
+            int[] P = new int[size];
+            int[] Q = new int[size];
+            for(int i = 0;i < size;++i)
+            {
+                P[i] = 1;
+                Q[i] = 50_000 - i;
+            }
+
+            s.solution(50_000, P, Q);
+        }
+
+    }
+}
---
+++ Codility.Lessons/Lesson11b/Solution.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.Lesson11b
+{
+    public class Solution
+    {
+        int[] sf = null;
+        bool[] s = null;
+        public int[] solution(int N, int[] P, int[] Q)
+        {
+            int[] R = new int[P.Length];
+            int[] prefix = new int[N + 1];
+            sieve(N);
+            factors(N);
+
+            for(int i = 2; i <= N; ++ i)
+            {
+                prefix[i] = prefix[i - 1] + (IsPrime(i) ? 1 : 0);
+            }
+
+            for(int i = 0;i < P.Length; ++i)
+            {
+                int p = P[i];
+                int q = Q[i];
+
+                int sum = prefix[q];
+                sum -= prefix[p - 1];
+
+                R[i] = sum;
+            }
+
+            return R;
+        }
+
+        public bool[] sieve(int N)
+        {
+            bool[] R = new bool[N + 1];
+            bool[] used = new bool[N + 1];
+
+            R[1] = false;
+
+            for(int i = 2; i <= N; ++i)
+            {
+                if (used[i])
+                    continue;
+                else
+                {
+                    int s = i;
+                    R[i] = true;
+                    used[i] = true;
+                    while (s <= N)
+                    {
+                        used[s] = true;
+                        s += i;
+                    }
+                }
+            }
+
+            return s = R;
+        }
+
+        public int[] factors(int N)
+        {
+            int[] smallestFactor = new int[N + 1];
+            bool[] used = new bool[N + 1];
+            smallestFactor[0] = 0;
+            smallestFactor[1] = 1;
+
+
+            for (int i = 2; i <= N; ++i)
+            {
+                if (used[i])
+                    continue;
+                else
+                {
+                    int s = i;
+                    smallestFactor[i] = i;
+                    used[i] = true;
+                    while (s <= N)
+                    {
+                        if (used[s] == false)
+                        {
+                            smallestFactor[s] = i;
+                            used[s] = true;
+                        }
+                        s += i;
+                    }
+                }
+            }
+
+            return sf = smallestFactor;
+        }
+
+        public bool IsPrime(int X)
+        {
+            int div1 = sf[X];
+            int div2 = sf[X / div1];
+
+            return s[div1] && s[div2] && div1 * div2 == X;
+        }
+
+
+        public int[] brute(int N, int[] P, int []Q)
+        {
+            int[] R = new int[P.Length];
+            sieve(N);
+            factors(N);
+
+            for(int i = 0; i < P.Length;++i)
+            {
+                int p = P[i];
+                int q = Q[i];
+                int fs = 0;
+                for (int x = p; x <= q; ++x)
+                    if (IsPrime(x))
+                        fs++;
+                R[i] = fs;
+            }
+
+            return R;
+        }
+        
+    }
+}
