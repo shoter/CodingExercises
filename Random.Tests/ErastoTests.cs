557ed91c05674e4a8296230e12dd8adb2225225c
shoter <lubiebardzozelki@gmail.com>
2020-05-29 23:40:32 +0200

    Added erasto

---
+++ Random.Tests/ErastoTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Misc.Tests
+{
+    public class ErastoTests
+    {
+        [Theory]
+        [InlineData(10, new int[] { 1, 2, 3, 5, 7 })]
+        public void test(int N, int[] R)
+        {
+            int[] r = new Erasto().solution(N);
+
+            foreach (int x in r)
+            {
+                R.Single(o => o == x);
+            }
+        }
+
+        [Fact]
+        public void longS()
+        {
+            new Erasto().solution(1_000_000);
+        }
+
+    }
+}
---
+++ Random/Erasto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc
+{
+    public class Erasto
+    {
+        public int[] solution(int N)
+        {
+            bool[] odd = new bool[N + 1];
+            bool[] isFirst = new bool[N + 1];
+
+            odd[1] = true;
+
+            for(int i = 2; i <= N; ++i)
+            {
+                if (odd[i])
+                    continue;
+                else
+                {
+                    isFirst[i] = true;
+                    for(int j = i * 2; j <= N; j += i)
+                    {
+                        odd[j] = true;
+                    }
+                }
+            }
+
+            List<int> result = new List<int>();
+            for (int i = 1; i <= N; ++i)
+                if (isFirst[i])
+                    result.Add(i);
+
+            return result.ToArray();
+        }
+        
+    }
+}
