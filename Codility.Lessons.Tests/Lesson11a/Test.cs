7fa11250984f65e851bf0eaed7462f8eca731c33
shoter <lubiebardzozelki@gmail.com>
2020-05-30 00:19:06 +0200

    l11at

---
+++ Codility.Lessons.Tests/Lesson11a/Test.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Lessons.Tests.Lesson11a
+{
+    public class Test
+    {
+        Codility.Lessons.Lesson11a.Solution s { get; } = new Lessons.Lesson11a.Solution();
+
+        [Theory]
+        [InlineData(new int[] { 3, 1, 2, 3, 6 }, new int[] { 2, 4, 3, 2, 0 })]
+        [InlineData(new int[] { 100 }, new int[] { 0 })]
+        [InlineData(new int[] { 2, 5 }, new int[] { 1, 1 })]
+        [InlineData(new int[] { 3, 6, 9 }, new int[] { 2, 1, 1 })]
+
+        [InlineData(new int[] { 9, 6, 9 }, new int[] { 1, 2, 1 })]
+
+        [InlineData(new int[] { 1, 2, 3, 4, 5}, new int[] { 4, 3, 3, 2, 3 })]
+
+
+        public void test(int[] A, int[] R)
+        {
+            Assert.Equal(R, s.solution(A));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 3, 1, 2, 3, 6 }, new int[] { 2, 4, 3, 2, 0 })]
+        [InlineData(new int[] { 100 }, new int[] { 0 })]
+        [InlineData(new int[] { 2, 5 }, new int[] { 1, 1 })]
+        [InlineData(new int[] { 3, 6, 9 }, new int[] { 2, 1, 1 })]
+        public void testB(int[] A, int[] R)
+        {
+            Assert.Equal(R, s.brute(A));
+        }
+
+        [Fact]
+        public void autoCheck()
+        {
+            for (int x = 1; x <= 1000; ++x)
+            {
+                int[] A = new int[x];
+                Random r = new Random();
+                for (int i = 0; i < x; ++i)
+                    A[i] = r.Next(1, x * 2);
+
+                Assert.Equal(s.brute(A), s.solution(A));
+            }
+        }
+
+
+        [Fact]
+        public void big()
+        {
+            for (int j = 0; j < 10; ++j)
+            {
+                int x = 50_000;
+                int[] A = new int[x];
+                Random r = new Random();
+                for (int i = 0; i < x; ++i)
+                    A[i] = r.Next(1, x * 2);
+
+                s.solution(A);
+            }
+        }
+
+        [Fact]
+        public void big2()
+        {
+            for (int j = 0; j < 1; ++j)
+            {
+                int x = 50_000;
+                int[] A = new int[x];
+                Random r = new Random();
+                for (int i = 0; i < x - 1; ++i)
+                    A[i] = r.Next(1,4);
+
+                A[x - 1] = x * 2;
+
+                s.solution(A);
+            }
+        }
+
+
+
+    }
+}
---
+++ Codility.Lessons/Lesson11a/Solution.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.Lesson11a
+{
+    public class Solution
+    {
+        public int[] solution(int[] A)
+        {
+            Dictionary<int, int> resultDick = new Dictionary<int, int>();
+            Dictionary<int, int> strength = new Dictionary<int, int>();
+            List<int> sorted = A.Distinct().ToList();
+            sorted.Sort();
+
+            for(int i = 0; i < A.Length; ++i)
+            {
+                resultDick[A[i]] = A.Length;
+                if (strength.ContainsKey(A[i]) == false)
+                    strength[A[i]] = 0;
+
+                strength[A[i]] += 1;
+            }
+
+            for(int i = 0; i < sorted.Count; ++i)
+            {
+                int s = sorted[i];
+                int l = sorted.Last();
+                int str = strength[s];
+
+                /*if (i > 0 && i != sorted.Count - 1 && s == sorted[i - 1])
+                    continue;*/
+
+                while(s <= l)
+                {
+                    if(resultDick.ContainsKey(s))
+                    {
+                        resultDick[s] -= str;
+                    }
+                    s += sorted[i];
+                }
+            }
+
+            int[] R = new int[A.Length];
+            for(int i = 0;i < A.Length; ++i)
+            {
+                R[i] = resultDick[A[i]];
+            }
+
+            return R;
+        }
+
+        public int[] brute(int[] A)
+        {
+            int[] R = new int[A.Length];
+            for(int i =0;i < A.Length; ++i)
+            {
+                int a = A[i];
+                int result = 0;
+                for(int j = 0; j < A.Length; ++j)
+                {
+                    if (i == j)
+                        continue;
+                    int b = A[j];
+                    
+                    if(a % b != 0)
+                    {
+                        result++;
+                    }
+
+                }
+
+                R[i] = result;
+            }
+
+            return R;
+        }
+        
+    }
+}
