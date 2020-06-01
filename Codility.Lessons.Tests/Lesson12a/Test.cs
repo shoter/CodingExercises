172fc758646e76408748b5ee39c8fda87d80f895
shoter <lubiebardzozelki@gmail.com>
2020-06-01 23:11:24 +0200

    l12a

---
+++ Codility.Lessons.Tests/Lesson12a/Test.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Lessons.Tests.Lesson12a
+{
+    public class Test
+    {
+        Codility.Lessons.Lesson12a.Solution s { get; } = new Lessons.Lesson12a.Solution();
+
+        [Theory]
+        [InlineData(10, 4, 5)]
+        public void test(int N, int M, int R) => Assert.Equal(R, s.solution(N, M));
+
+        [Theory]
+        [InlineData(10, 4, 5)]
+        public void bruteTest(int N, int M, int R) => Assert.Equal(R, s.brute(N, M));
+
+        [Fact]
+        public void autoCheck()
+        {
+            for(int n = 1; n <= 1000; ++n)
+            {
+                for(int m = 1; m <= 1000; ++m)
+                {
+                    Assert.Equal(s.brute(n, m), s.solution(n, m));
+                }
+            }
+        }
+
+        [Fact]
+        public void lastIndexCheck()
+        {
+            for (int n = 1; n <= 100; ++n)
+            {
+                for (int m = 1; m <= 100; ++m)
+                {
+                    Assert.Equal(0, s.lastIndex(n, m));
+                }
+            }
+        }
+
+        [Fact]
+        public void big()
+        {
+            s.solution(1_000_000_000, 1_000_000_000);
+        }
+
+        [Fact]
+        public void big2()
+        {
+            s.solution(1_000_000_002, 1_000_000_001);
+        }
+
+
+
+
+    }
+}
---
+++ Codility.Lessons/Lesson12a/Solution.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.Lesson12a
+{
+    public class Solution
+    {
+        public int solution(int N, int M)
+        {
+            int max = Math.Max(N, M);
+            int min = Math.Min(N, M);
+
+            int g = gcd(max, min);
+
+            return N / g;
+        }
+
+        public int brute(int N, int M)
+        {
+            bool[] eaten = new bool[N];
+
+            int index = 0;
+            int count = 0;
+            while(eaten[index] == false)
+            {
+                eaten[index] = true;
+                count++;
+
+                index += M;
+                index %= N;
+            }
+
+            return count;
+        }
+
+        public int gcd(int a, int b)
+        {
+            // a bigger than b
+            if (a % b == 0)
+                return b;
+            return gcd(b, a % b);
+        }
+
+        public int lastIndex(int N, int M)
+        {
+            bool[] eaten = new bool[N];
+
+            int index = 0;
+            int count = 0;
+            while (eaten[index] == false)
+            {
+                eaten[index] = true;
+                count++;
+
+                index += M;
+                index %= N;
+            }
+
+            return index;
+        }
+    }
+}
