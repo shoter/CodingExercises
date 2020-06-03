a0a36901785a95cc7babdb07222e26a403167258
shoter <lubiebardzozelki@gmail.com>
2020-06-04 01:36:29 +0200

    l13b

---
+++ Codility.Lessons.Tests/Lesson13b/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Lessons.Tests.Lesson13b
+{
+    public class Test
+    {
+        Codility.Lessons.Lesson13b.Solution s = new Lessons.Lesson13b.Solution();
+        [Theory]
+        [InlineData(new int[] { 4, 4, 5, 5, 1 }, new int[] { 3, 2, 4, 3, 1 }, new int[] { 5, 1, 8, 0, 1 })]
+        public void test(int[] A, int [] B, int[] R)
+        {
+            Assert.Equal(R, s.solution(A, B));
+
+        }
+
+        [Fact]
+        public void big()
+        {
+            int[] A = new int[50_000];
+            int[] B = new int[50_000];
+
+            for(int i = 0;i < 50_000; ++i)
+            {
+                A[i] = 50_000 - i;
+                B[i] = 30;
+            }
+
+            int[] R = s.solution(A, B);
+
+            foreach (var r in R)
+                Assert.True(r > 0);
+        }
+        
+    }
+}
---
+++ Codility.Lessons/Lesson13b/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.Lesson13b
+{
+    public class Solution
+    {
+        public int[] solution(int[] A, int [] B)
+        {
+            int[] possibilities = new int[50001];
+            int maxRungs = 2;
+            possibilities[1] = 1;
+            possibilities[2] = 2;
+            int[] R = new int[A.Length];
+
+            int maxPowa = 1;
+            for (int x = 0; x < 30; ++x)
+                maxPowa *= 2;
+
+            for (int p = 3; p <= 50000; ++p)
+                possibilities[p] = (possibilities[p - 1] + possibilities[p - 2]) % maxPowa;
+
+            for(int i = 0; i < A.Length; ++i)
+            {
+                int rungs = A[i];
+
+                int pow = 1;
+
+                for (int x = 0; x < B[i]; ++x)
+                    pow *= 2;
+
+               /* for(int x = 3; x <= rungs; ++x)
+                {
+                    possibilities[x] = (possibilities[x - 1] + possibilities[x - 2]) % pow;
+                }*/
+
+                R[i] = possibilities[rungs] % pow;
+            }
+
+            return R;
+
+        }
+        
+    }
+}
