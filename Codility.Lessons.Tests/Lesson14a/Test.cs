e9d3f2cb7861d2cddffba139d5c693056bf29688
shoter <lubiebardzozelki@gmail.com>
2020-06-04 15:42:09 +0200

    l14a

---
+++ Codility.Lessons.Tests/Lesson14a/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Lessons.Tests.Lesson14a
+{
+    public class Test
+    {
+        Codility.Lessons.Lesson14a.Solution s = new Lessons.Lesson14a.Solution();
+
+        [Theory]
+        [InlineData(3, 5, new int[] { 2, 1, 5, 1, 2, 2, 2 }, 6)]
+        [InlineData(2, 5, new int[] { 1,2,4,7,8,9}, 17)]
+        [InlineData(3, 5, new int[] { 1, 2, 4, 7, 8, 9 }, 14)]
+        [InlineData(4, 5, new int[] { 1, 2, 4, 7, 8, 9 }, 9)]
+        public void test(int K, int M, int[] A, int R)
+        {
+            Assert.Equal(R, s.solution(K, M, A));
+        }
+        
+    }
+}
---
+++ Codility.Lessons/Lesson14a/Solution.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.Lesson14a
+{
+    public class Solution
+    {
+        public int solution(int K, int M, int[] A)
+        {
+            if (A.Length == 1)
+                return A[0];
+
+            int maxSum = 0;
+            int minSum = int.MaxValue;
+
+            for (int i = 0; i < A.Length; ++i)
+            {
+                maxSum += A[i];
+                minSum = Math.Min(minSum, A[i]);
+            }
+
+            while (maxSum != minSum)
+            {
+                int midSum = (maxSum + minSum) / 2;
+
+                bool ok = isDividable(K, midSum, A);
+
+                if(ok)
+                {
+                    maxSum = midSum;
+                }
+                else
+                {
+                    minSum = midSum + 1;
+                }
+            }
+            return maxSumCalc(K, maxSum, A);
+        }
+
+        public bool isDividable(int K, int mid, int[] A)
+        {
+            int blocksLeft = K;
+            int curSum = 0;
+            for (int i = 0; i < A.Length; ++i)
+            {
+                curSum += A[i];
+
+                if (curSum > mid)
+                {
+                    blocksLeft--;
+                    curSum = A[i];
+                }
+
+                if (blocksLeft == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public int maxSumCalc(int K, int mid, int[] A)
+        {
+            int blocksLeft = K;
+            int curSum = 0;
+            int maxSum = 0;
+            for (int i = 0; i < A.Length; ++i)
+            {
+                maxSum = Math.Max(curSum, maxSum);
+                curSum += A[i];
+
+                if (curSum > mid)
+                {
+                    blocksLeft--;
+                    curSum = A[i];
+                }
+
+                if (blocksLeft == 0)
+                {
+                    throw new Exception("WUT?");
+                }
+            }
+
+            return Math.Max(maxSum, curSum);
+        }
+
+
+    }
+}
