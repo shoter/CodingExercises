a6d4db16c90842378c5bc225474a8f2e9472f070
shoter <lubiebardzozelki@gmail.com>
2020-06-08 01:59:25 +0200

    l14b progress

---
+++ Codility.Lessons.Tests/Lesson14b/Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Lessons.Tests.Lesson14b
+{
+    public class Test
+    {
+        Codility.Lessons.Lesson14b.Solution s { get; } = new Lessons.Lesson14b.Solution();
+
+        [Theory]
+        [InlineData(new int[] { 1, 4, 5, 8}, new int[] { 4, 5, 9, 10}, new int[] { 4, 6, 7, 10, 2 }, 4)]
+        [InlineData(new int[] { 1, 2 }, new int[] { 2, 3 }, new int[] { 2, 1, 3 }, 1)]
+        [InlineData(new int[] { 1, 2 }, new int[] { 2, 3 }, new int[] { 1, 3, 2 }, 2)]
+        [InlineData(new int[] { 1, 2 }, new int[] { 2, 3 }, new int[] { 1 }, -1)]
+        [InlineData(new int[] { 1, 2 }, new int[] { 2, 3 }, new int[] { 3 }, -1)]
+        public void test(int[] A, int[] B, int [] C, int R)
+        {
+            Assert.Equal(R, s.solution(A, B, C));
+        }
+
+        [Fact]
+        public void big()
+        {
+            int size = 30_000;
+            int[] A = new int[size];
+            int[] B = new int[size];
+            int[] C = new int[size];
+
+            Random r = new Random();
+            for(int i = 0;i < size; ++i)
+            {
+                A[i] = r.Next(1, 1000);
+                B[i] = r.Next(1000, 2000);
+                C[i] = r.Next(1, 2000);
+            }
+
+            s.solution(A, B, C);
+        }
+        
+    }
+}
---
+++ Codility.Lessons/Lesson14b/Solution.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.Lesson14b
+{
+    public class Solution
+    {
+        public struct Nail : IComparable
+        {
+            public int id;
+            public int position;
+
+            public Nail(int id, int pos)
+            {
+                this.id = id;
+                this.position = pos;
+            }
+
+            public int CompareTo(object obj)
+            {
+                Nail other = (Nail)obj;
+                return position.CompareTo(other.position);
+            }
+        }
+
+
+        List<int>[] nailPlanks;
+        public int solution(int[] A, int[] B, int[] C)
+        {
+            List<Nail> nails = new List<Nail>(C.Length);
+
+            for (int i = 0; i < C.Length; ++i)
+                nails.Add(new Nail(i, C[i]));
+
+            nails.Sort();
+
+            int earliestPossible = FindFirstPossibleNail(A[0], B[0], nails, -1);
+
+            for (int i = 1; i < A.Length; ++i)
+            {
+                if (earliestPossible == int.MaxValue)
+                    return -1;
+
+                earliestPossible = FindFirstPossibleNail(A[i], B[i], nails, earliestPossible);
+            }
+
+            if (earliestPossible == int.MaxValue)
+                return -1;
+
+            return earliestPossible + 1;
+        }
+
+        public int FindFirstPossibleNail(int start, int end, List<Nail> nails, int previousResult)
+        {
+            int min = 0;
+            int max = nails.Count - 1;
+            int earliest = int.MaxValue;
+
+            while(min <= max)
+            {
+                int mid = (min + max) / 2;
+
+                if(nails[mid].position < start)
+                {
+                    min = mid + 1;
+                    
+                }
+                else if(nails[mid].position > end)
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    earliest = mid;
+                    max = mid - 1;
+                }
+            }
+
+            if (earliest == int.MaxValue)
+                return int.MaxValue;
+
+            int earliestIndex = nails[earliest].id;
+
+            for(int i = earliest + 1; i < nails.Count; ++i)
+            {
+                if (nails[i].position > end)
+                    return Math.Max(earliestIndex, previousResult);
+
+                earliestIndex = Math.Min(earliestIndex, nails[i].id);
+
+                if (earliestIndex < previousResult)
+                    return previousResult;
+            }
+
+            return Math.Max(earliestIndex, previousResult);
+        }
+    }
+}
