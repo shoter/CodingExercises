49cb8866c75be7f31fa6ab3638b228079413d087
shoter <lubiebardzozelki@gmail.com>
2020-06-09 01:59:29 +0200

    l15a

---
+++ Codility.Lessons.Tests/Lesson15a/Test.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Lessons.Tests.Lesson15a
+{
+    public class Test
+    {
+        Codility.Lessons.Lesson15a.Solution s { get; } = new Lessons.Lesson15a.Solution();
+
+        [Theory]
+        [InlineData(new int[] { -5, -3, -1, 0, 3, 6}, 5)]
+        [InlineData(new int[] { 0 }, 1)]
+        [InlineData(new int[] { int.MaxValue }, 1)]
+        [InlineData(new int[] { int.MinValue }, 1)]
+        [InlineData(new int[] { int.MinValue, int.MaxValue }, 2)]
+        [InlineData(new int[] { int.MinValue, 0, int.MaxValue }, 3)]
+        [InlineData(new int[] { int.MinValue, int.MinValue, int.MinValue ,0, int.MaxValue }, 3)]
+        [InlineData(new int[] { int.MinValue, int.MinValue, -5 ,0, int.MaxValue }, 4)]
+        [InlineData(new int[] { int.MinValue, 0, 0, int.MaxValue }, 3)]
+        [InlineData(new int[] { int.MinValue, 0, 0, 0 ,int.MaxValue }, 3)]
+        public void test(int[] A, int R)
+        {
+            Assert.Equal(R, s.solution(A));
+        }
+
+        [Theory]
+        [InlineData(new int[] { -5, -3, -1, 0, 3, 6 }, 5)]
+        [InlineData(new int[] { 0 }, 1)]
+        [InlineData(new int[] { int.MaxValue }, 1)]
+        [InlineData(new int[] { int.MinValue }, 1)]
+        [InlineData(new int[] { int.MinValue, int.MaxValue }, 2)]
+        [InlineData(new int[] { int.MinValue, 0, int.MaxValue }, 3)]
+        [InlineData(new int[] { int.MinValue, int.MinValue, int.MinValue, 0, int.MaxValue }, 3)]
+        [InlineData(new int[] { int.MinValue, int.MinValue, -5, 0, int.MaxValue }, 4)]
+        [InlineData(new int[] { int.MinValue, 0, 0, int.MaxValue }, 3)]
+        [InlineData(new int[] { int.MinValue, 0, 0, 0, int.MaxValue }, 3)]
+        public void testBrute(int[] A, int R)
+        {
+            Assert.Equal(R, s.brute(A));
+        }
+
+        [Fact]
+        public void big()
+        {
+            int[] A = new int[100_000];
+            Random r = new Random();
+            for (int i = 0; i < A.Length; ++i)
+                A[i] = -1 * A.Length + 2 * i;
+
+            s.solution(A);
+        }
+
+        [Fact]
+        public void autoCheck()
+        {
+            for(int x = 1; x <= 10_00; ++x)
+            {
+                for (int a = 0; a < 10; ++a)
+                {
+                    int[] A = new int[x];
+                    Random r = new Random();
+                    for (int i = 0; i < x; ++i)
+                        A[i] = r.Next(-1 * x, x);
+
+                    List<int> temp = A.ToList();
+
+                    temp.Sort();
+
+                    A = temp.ToArray();
+
+                    Assert.Equal(s.brute(A), s.solution(A));
+                }
+            }
+        }
+
+    }
+}
---
+++ Codility.Lessons/Lesson15a/Solution.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.Lesson15a
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            int left = 0;
+            int right = A.Length - 1;
+
+            int? pl = null;
+            int? pr = null;
+
+            int count = 0;
+
+            while (left <= right)
+            {
+                if (A[left] == int.MinValue)
+                {
+                    if (A[left] == pl)
+                    {
+                        left++;
+                        continue;
+                    }
+                    left++;
+                    count += 1;
+                    pl = int.MinValue;
+                    continue;
+                }
+
+                int l = Math.Abs(A[left]);
+                int r = Math.Abs(A[right]);
+
+                if (l == pl)
+                {
+                    left++;
+                    continue;
+                }
+                if (r == pr)
+                {
+                    right--;
+                    continue;
+                }
+
+                 if (l > r)
+                {
+                    count += 1;
+                    left++;
+                    pl = l;
+                }
+                else if (r > l)
+                {
+                    count += 1;
+                    right--;
+                    pr = r;
+
+                }
+                else if (l == r) // for clarity
+                {
+                    left++;
+                    right--;
+                    ++count;
+                    pl = l;
+                    pr = r;
+                }
+            }
+
+            return count;
+
+        }
+
+        public int brute(int[] A)
+        {
+            HashSet<uint> uniques = new HashSet<uint>();
+            foreach (var a in A)
+                uniques.Add((uint)Math.Abs((long)a));
+
+            return uniques.Count;
+        }
+
+    }
+}
