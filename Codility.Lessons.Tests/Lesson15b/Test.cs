0af41ae236234fb57bc150fe6602b4331f56b718
shoter <lubiebardzozelki@gmail.com>
2020-06-09 02:38:02 +0200

    l15b

---
+++ Codility.Lessons.Tests/Lesson15b/Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Lessons.Tests.Lesson15b
+{
+    public class Test
+    {
+        Codility.Lessons.Lesson15b.Solution s { get; } = new Lessons.Lesson15b.Solution();
+
+        [Theory]
+        [InlineData(6, new int[] { 3, 4, 5, 5, 2}, 9)]
+        [InlineData(6, new int[] { 6 }, 1)]
+        [InlineData(6, new int[] { 6, 6, 6 }, 3)]
+        [InlineData(6, new int[] { 6, 6, 2 }, 4)]
+        [InlineData(6, new int[] { 6, 6, 2, 2 }, 5)]
+
+
+        public void test(int M, int [] A, int R)
+        {
+            Assert.Equal(R, s.solution(M, A));
+        }
+
+        [Fact]
+        public void big()
+        {
+            int sum = 0;
+
+            for(int i = 0;i < 100_000; ++i)
+            {
+                sum += (100_000) - i;
+            }
+
+            int[] A = new int[100_000];
+            for (int i = 0; i < 100_000; ++i)
+                A[i] = i;
+
+            Assert.Equal(sum, s.solution(99_999, A));
+        }
+        
+    }
+}
---
+++ Codility.Lessons/Lesson15b/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.Lesson15b
+{
+    public class Solution
+    {
+        const int max = 1000000000;
+        public int solution(int M, int [] A)
+        {
+            int left = 0;
+            int right = 0;
+
+            Dictionary<int, bool> used = new Dictionary<int, bool>();
+
+            for (int i = 0; i <= M; ++i)
+                used.Add(i, false);
+
+            used[A[0]] = true;
+            int count = 0;
+
+            while(left < A.Length)
+            {
+                if (count > max)
+                    return max;
+                if (right == A.Length - 1)
+                {
+                    count += right - left + 1;
+                    left++;
+                    continue;
+                }
+
+                int candidate = A[right + 1];
+                
+                if(!used[candidate])
+                {
+                    used[candidate] = true;
+                    right++;
+                }
+                else
+                {
+                    count += right - left + 1;
+                    used[A[left]] = false;
+                    left++;
+                }
+            }
+
+            return Math.Min(count, max);
+        }
+        
+    }
+}
