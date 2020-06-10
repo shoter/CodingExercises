8aa428d05046a3d9fa0bc6d4633b008f7f1a7838
shoter <lubiebardzozelki@gmail.com>
2020-06-10 23:18:30 +0200

    l15c

---
+++ Codility.Lessons.Tests/Lesson15c/Test.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Lessons.Tests.Lesson15c
+{
+    public class Test
+    {
+        Codility.Lessons.Lesson15c.Solution s = new Lessons.Lesson15c.Solution();
+
+        [Theory]
+        [InlineData(new int[] { 10, 2, 5, 1, 8, 12 }, 4)]
+        [InlineData(new int[] { 4, 5, 3 }, 1)]
+        [InlineData(new int[] { 4, 5 }, 0)]
+        [InlineData(new int[] { }, 0)]
+        [InlineData(new int[] { 4 }, 0)]
+        [InlineData(new int[] { 6, 6, 4, 5 }, 4)]
+        [InlineData(new int[] { 6, 3, 1, 6 }, 2)]
+
+        public void test(int[] A, int R)
+        {
+            Assert.Equal(R, s.solution(A));
+        }
+
+        [Fact]
+        public void autoCheck()
+        {
+            for (int x = 0; x <= 50; ++x)
+            {
+                for (int j = 0; j < 10; ++j)
+                {
+                    int[] A = new int[x];
+                    Random r = new Random();
+                    for (int i = 0; i < x; ++i)
+                        A[i] = r.Next(1, x * 2);
+
+                    Assert.Equal(s.brute(A), s.solution(A));
+                }
+            }
+        }
+
+        [Fact]
+        public void big()
+        {
+
+            for (int x = 0; x <= 10; ++x)
+            {
+                int[] A = new int[1000];
+                Random r = new Random();
+                for (int i = 0; i < 1000; ++i)
+                    A[i] = r.Next(1, 1_000_000);
+
+                s.solution(A);
+
+            }
+        }
+
+        [Theory]
+        [InlineData(new int[] { 10, 2, 5, 1, 8, 12 }, 4)]
+        [InlineData(new int[] { 6,3,1,6}, 2)]
+
+        public void bruteTest(int[] A, int R)
+        {
+            Assert.Equal(R, s.brute(A));
+        }
+
+    }
+}
---
+++ Codility.Lessons/Lesson15c/Solution.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.Lesson15c
+{
+    public class Solution
+    {
+        public struct Number : IComparable
+        {
+            public int value;
+            public int pos;
+
+            public Number(int v, int p)
+            {
+                value = v;
+                pos = p;
+            }
+
+            public int CompareTo(object obj)
+            {
+                Number other = (Number)obj;
+                return value.CompareTo(other.value);
+            }
+
+            public override string ToString() => $"{value} - {pos}";
+        }
+
+        public int solution(int[] A)
+        {
+            if (A.Length <= 2)
+                return 0;
+
+            List<Number> Numbers = new List<Number>(A.Length);
+            for (int i = 0; i < A.Length; ++i)
+                Numbers.Add(new Number(A[i], i));
+
+            Numbers.Sort();
+
+            int count = 0;
+            for(int p = 0; p < A.Length - 2; ++p)
+            {
+                int nc =  possibleSolutions(Numbers, p);
+
+                if (nc == -1)
+                    continue;
+                count += nc;
+            }
+
+            return count;
+        }
+
+        public int possibleSolutions(List<Number> numbers, int p)
+        {
+            int count = 0;
+            int previousR = -1;
+            for(int q = p + 1; q < numbers.Count - 1; ++ q)
+            {
+                // pomiedzy tymi indexami znajduje sie pierwsze r.
+                int max = numbers.Count - 1;
+                int min = Math.Max(previousR, q + 1);
+
+                int rMaxValue = numbers[p].value + numbers[q].value - 1;
+                int r = findHighestPossibleR(numbers, min, max, rMaxValue);
+
+                if (r != -1)
+                {
+                    previousR = r;
+                    Debug.Assert(r > q);
+                    Debug.Assert(r < numbers.Count);
+
+                    count += (r - q);
+                }
+
+            }
+
+            return count;
+        }
+
+        public int findHighestPossibleR(List<Number> numbers, int min, int max, int maxValue)
+        {
+            int lastR = -1;
+            while(min <= max)
+            {
+                int mid = (min + max) / 2;
+                bool ok = numbers[mid].value <= maxValue;
+
+                if(ok)
+                {
+                    lastR = mid;
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid - 1;
+                }
+            }
+
+            return lastR;
+        }
+
+
+
+
+
+        public int brute(int[] A)
+        {
+            int count = 0;
+            for(int i = 0;i < A.Length; ++i)
+            {
+                for(int j = i + 1;j < A.Length; ++j)
+                {
+                    for(int k = j + 1; k < A.Length; ++k)
+                    {
+                        int a = A[i];
+                        int b = A[j];
+                        int c = A[k];
+
+                        if (a + b > c && a + c > b && b + c > a)
+                            count++; 
+                    }
+                }
+            }
+
+            return count;
+        }
+        
+    }
+}
