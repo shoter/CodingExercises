855318d1a326bedb82cd74ca81a2407b196de136
shoter <lubiebardzozelki@gmail.com>
2020-06-10 23:54:15 +0200

    l15d done

---
+++ Codility.Lessons.Tests/Lesson15d/Test.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Lessons.Tests.Lesson15d
+{
+    public class Test
+    {
+        Codility.Lessons.Lesson15d.Solution s = new Lessons.Lesson15d.Solution();
+        [Theory]
+        [InlineData(new int[] { 1, 4, -3 }, 1)]
+        [InlineData(new int[] { -8, 4, 5, -10, 3 }, 3)]
+        public void test(int[] A, int R)
+        {
+            Assert.Equal(R, s.solution(A));
+        }
+
+        [Fact]
+        public void autoCheck()
+        {
+            for (int x = 1; x < 100; ++x)
+            {
+                for (int j = 105; j > x; --j)
+                {
+                    int[] A = new int[x];
+                    Random r = new Random();
+                    for (int i = 0; i < x; ++i)
+                        A[i] = r.Next(-50, 50);
+
+                    Assert.Equal(s.brute(A), s.solution(A));
+                }
+            }
+        }
+
+        [Fact]
+        public void big()
+        {
+            for (int x = 1; x < 10; ++x)
+            {
+                int[] A = new int[100_000];
+                Random r = new Random();
+                for (int i = 0; i < A.Length; ++i)
+                    A[i] = r.Next(-100, 100);
+                s.solution(A);
+            }
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 4, -3 }, 1)]
+        [InlineData(new int[] { -8, 4, 5, -10, 3 }, 3)]
+        public void bruteTest(int[] A, int R)
+        {
+            Assert.Equal(R, s.brute(A));
+        }
+
+
+
+    }
+}
---
+++ Codility.Lessons/Lesson15d/Solution.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.Lesson15d
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            List<int> numbers = new List<int>(A.Length);
+            for (int i = 0; i < A.Length; ++i)
+                numbers.Add(A[i]);
+
+            numbers.Sort();
+
+            if (numbers[0] >= 0)
+                return numbers[0] * 2;
+            if (numbers[numbers.Count - 1] <= 0)
+                return (-numbers[numbers.Count - 1] * 2);
+
+            int max = A.Length - 1;
+            int min = 0;
+
+            int minAbs = Math.Min(Math.Abs(numbers[min] * 2), Math.Abs(numbers[max] * 2));
+            while(min <= max)
+            {
+                int nmin = numbers[min];
+                int nmax = numbers[max];
+
+                if (nmin > 0)
+                    break;
+                if (nmax < 0)
+                    break;
+
+                minAbs = Math.Min(minAbs, Math.Abs(nmax + nmin));
+
+                if(nmax >= -nmin)
+                {
+                    max--;
+                    minAbs = Math.Min(minAbs, Math.Abs(numbers[max] * 2));
+                }
+                else
+                {
+                    min++;
+                    minAbs = Math.Min(Math.Abs(numbers[min] * 2), minAbs);
+
+                }
+            }
+
+            return minAbs;
+        }
+
+        public int brute(int[] A)
+        {
+            int minAbs = int.MaxValue;
+
+            for(int i = 0;i < A.Length; ++i)
+                for(int j = 0;j < A.Length; ++j)
+                {
+                    minAbs = Math.Min(Math.Abs(A[i] + A[j]), minAbs);
+                }
+
+            return minAbs;
+        }
+        
+    }
+}
