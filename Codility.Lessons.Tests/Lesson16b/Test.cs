213c0e42c8d733af7a3be897bb4d6b80a92d91df
shoter <lubiebardzozelki@gmail.com>
2020-06-12 01:02:40 +0200

    l16b

---
+++ Codility.Lessons.Tests/Lesson16b/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Lessons.Tests.Lesson16b
+{
+    public class Test
+    {
+        Codility.Lessons.Lesson16b.Solution s { get; } = new Lessons.Lesson16b.Solution();
+
+        [Theory]
+        [InlineData(4, new int[] { 1, 2, 3, 4, 1, 1, 3 }, 3)]
+        [InlineData(6, new int[] { 8, 4, 4, 3, 3, 1, 4, 3 }, 4)]
+        [InlineData(6, new int[] { 6 }, 1)]
+        [InlineData(6, new int[] { 5 }, 0)]
+        [InlineData(1_000_000_000, new int[] { 999_999_999, 999_999_999, 999_999_999}, 1)]
+
+
+        public void test(int K, int [] A, int R)
+        {
+            Assert.Equal(R, s.solution(K, A));
+        }
+        
+    }
+}
---
+++ Codility.Lessons/Lesson16b/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.Lesson16b
+{
+    public class Solution
+    {
+        public int solution(int K, int[] A)
+        {
+            int n = 0;
+            int count = 0;
+            for(int i =0;i < A.Length; ++i)
+            {
+                n += A[i];
+                if(n >= K)
+                {
+                    count++;
+                    n = 0;
+                }
+            }
+
+            return count;
+
+        }
+    }
+}
