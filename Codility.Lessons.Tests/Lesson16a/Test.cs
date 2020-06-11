b6cac9a440f62fb25bdbd93ed6d6c097b3fbe21e
shoter <lubiebardzozelki@gmail.com>
2020-06-12 00:48:13 +0200

    l16a

---
+++ Codility.Lessons.Tests/Lesson16a/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Lessons.Tests.Lesson16a
+{
+    public class Test
+    {
+        Codility.Lessons.Lesson16a.Solution s { get; } = new Lessons.Lesson16a.Solution();
+
+        [Theory]
+        [InlineData(new int[] { 1, 3, 7, 9, 9 }, new int[] { 5, 6, 8, 9, 10 }, 3)]
+        [InlineData(new int[] { 1}, new int[] { 2}, 1)]
+        [InlineData(new int[] { }, new int[] { }, 0)]
+
+        [InlineData(new int[] { 2, 7, 1, 9 }, new int[] { 8, 9, 10, 14 }, 2)]
+
+
+
+        public void test(int [] A, int [] B, int R)
+        {
+            Assert.Equal(R, s.solution(A, B));
+        }
+    }
+}
---
+++ Codility.Lessons/Lesson16a/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.Lesson16a
+{
+    public class Solution
+    {
+        public int solution(int[] A, int [] B)
+        {
+            if (A.Length == 0)
+                return 0;
+
+            int count = 0;
+            int current = 0;
+
+            for(int i = 1;i < A.Length; ++i)
+            {
+                int a2 = A[i];
+                int b = B[current];
+                int a = A[current];
+                int b2 = B[i];
+
+                // no colission - the last ount will increase count of segments.
+                if(a2 > b)
+                {
+                    count++;
+                    current = i;
+                }
+                else
+                {
+                    //collision - let's take one that end earlier.
+                    if(b2 < b)
+                    {
+                        current = i;
+                        // we dropped the initial one in this loop.
+
+                    }
+                }
+            }
+
+            //we have something that ended here so we increase count by 1
+            return count + 1;
+        }
+    }
+}
